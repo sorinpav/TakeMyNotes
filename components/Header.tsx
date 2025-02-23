35291a2f9720aa8d4f29367e15d08b6b20f8221f
Sorin Pavelescu <sorinpav93@outlook.com>
2025-02-23 21:10:35 +0000

    clerk basic setup + a basic minimally styled header

---
+++ components/Header.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import { useUser } from "@clerk/nextjs";
+
+const Header = () => {
+  const { user } = useUser();
+  return (
+    <div className="flex items-center justify-between p-5">
+      {user && (
+        <h1 className="text-2xl">
+          {user?.firstName}
+          {`'s`} Space
+        </h1>
+      )}
+
+      {/* Breadcrumbs */}
+
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <UserButton />
+        <SignedIn></SignedIn>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
